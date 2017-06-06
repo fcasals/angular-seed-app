95d78425b09d4ed264493afaeca46c5d4195710a
Francisco Casals <francisco.casals@gmail.com>
2017-06-06 00:08:42 -0400

    FIX: dependencia de ui-router

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -30,12 +30,12 @@ module.exports = function(config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
-      'bower_components/angular-ui-router/release/angular-ui-router.js',
       'bower_components/angular-growl-v2/build/angular-growl.js',
       'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
       'bower_components/moment/moment.js',
       'bower_components/moment/locale/es.js',
       'bower_components/angular-moment/angular-moment.js',
+      'bower_components/angular-ui-router/release/angular-ui-router.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
